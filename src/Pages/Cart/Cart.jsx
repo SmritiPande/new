ac985a3030ab3b9aa7d65aa5c2a3f44c7f500f5f
SmritiPande <smritipandey216@gmail.com>
2024-07-15 15:45:02 +0530

    first commit

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -44,7 +44,7 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>{getTotalCartAmount()}</p>
+              <p> {getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
